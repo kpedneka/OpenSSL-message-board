ce07737a068e95bbb6e5c573329e6730d248cdb2
noel <noeltaide@gmail.com>
2018-04-25 18:20:43 -0400

    Username/Password communication prototype

--- client.py
+++ client.py
@@ -7,13 +7,15 @@ client_ssl = ssl.wrap_socket(client_sock,
                              ca_certs="server.crt",
                              cert_reqs=ssl.CERT_REQUIRED)
 client_ssl.connect((SERVER, PORT))
-client_ssl.sendall(bytes("This is from Client".encode("UTF-8")))
+#client_ssl.sendall(bytes("This is from Client".encode("UTF-8")))
 while True:
     in_data = client_ssl.recv(1024)
     if in_data == 'CLOSE':
         break
     print("From Server :", in_data.decode())
-    out_data = raw_input()
+    out_data = ''
+    while(out_data == ''):
+        out_data = raw_input()
     client_ssl.sendall(bytes(out_data.encode("UTF-8")))
     if out_data == 'quit':
         break
--- server.py
+++ server.py
@@ -11,8 +11,41 @@ class ClientThread(threading.Thread):
     def run(self):
         while not self.kill_received:
             print ("Connection from : ", clientAddress)
+            auth = False;
+            valid_user = False
+            valid_password = False
+
             # self.csocket.send(bytes("Hi, This is from Server..",'utf-8'))
-            msg = ''
+            while auth is False:
+                while valid_user is False:
+                    msg = 'Please enter an alphanumeric username'
+                    self.client_ssl.send(bytes(msg.encode('UTF-8')))
+                    data = self.client_ssl.recv(2048)
+                    username = data.decode()
+                    if username.isalnum() is True:
+                        valid_user = True;
+
+                print("User entered username: ", username)
+                # search for username, store hash in variable 'password'
+
+                while valid_password is False:
+                    msg = 'Please enter an alphanumeric password'
+                    self.client_ssl.send(bytes(msg.encode('UTF-8')))
+                    data = self.client_ssl.recv(2048)
+                    password = data.decode()
+                    if password.isalnum() is True:
+                        valid_password = True;
+
+                auth = True
+                print("User ", username, " entered password ", password)
+
+                # if password is null, user must create account by entering password
+
+                # if password is not null, ask user to enter password, hash, and compare against our checked hash
+
+            msg = "Welcome, " + username
+            self.client_ssl.send(bytes(msg.encode('UTF-8')))
+
             while True:
                 data = self.client_ssl.recv(2048)
                 msg = data.decode()
